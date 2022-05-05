ea7f56a534d66da8878865237417a7adaba1ee90
Brookzerker <brooks@brookzerker.com>
2022-05-04 18:48:52 -0600

    added ability to logout

--- src/pages/authentication.rs
+++ src/pages/authentication.rs
@@ -2,9 +2,16 @@ use std::env;
 
 use rand_os::rand_core::RngCore;
 use yew::prelude::*;
+use yew_router::{
+    history::{self, Location},
+    hooks::{use_history, use_location},
+};
 use yewdux_functional::use_store;
 
-use crate::{utils::log_string, yewdux_store::AuthStoreType};
+use crate::{
+    utils::{create_random_string, log_string},
+    yewdux_store::AuthStoreType,
+};
 
 #[function_component(Authentication)]
 pub fn authentication() -> Html {
@@ -16,19 +23,29 @@ pub fn authentication() -> Html {
     let login_onclick = Callback::from(|_event: MouseEvent| {
         let login_url = create_login_url();
         log_string(&login_url);
-        gloo::utils::window().location().set_href(&login_url);
+        gloo::utils::window()
+            .location()
+            .set_href(&login_url)
+            .unwrap();
     });
+
+    let logout_onclick = Callback::from(move |_event: MouseEvent| {
+        let logout_url = create_logout_url("http://localhost:8080/authentication");
+        // this can be replaced with a use_navigator after yew_router 0.17 or later is released
+        gloo::utils::window()
+            .location()
+            .set_href(&logout_url)
+            .unwrap();
+    });
+
+    let location = use_location().unwrap();
+    log_string(&location.search().to_string());
 
     html! {
       <div>
         <h1>{"Authentication"}</h1>
-        if !auth_loading {
-          if is_authenticated {
-            <button>{"Logout"}</button>
-          } else {
+            <button onclick={logout_onclick}>{"Logout"}</button>
             <button onclick={login_onclick}>{"login"}</button>
-          }
-        }
       </div>
     }
 }
@@ -37,14 +54,13 @@ fn create_login_url() -> String {
     let domain = env!("AUTH0_DOMAIN");
     let client_id = env!("AUTH0_CLIENT_ID");
     let redirect_uri = "http://localhost:8080/authentication";
-    let mut random_numbers = [0u8; 16];
-
-    rand_os::rand_core::OsRng.fill_bytes(&mut random_numbers);
-    let mut state = random_numbers
-        .into_iter()
-        .map(|item: u8| item.to_string())
-        .collect::<Vec<String>>()
-        .join("");
+    let state = create_random_string();
 
     format!("https://{domain}/authorize?response_type=token&client_id={client_id}&redirect_uri={redirect_uri}&scope=openid%20profile%20email&state={state}")
+}
+
+fn create_logout_url(return_to: &str) -> String {
+    let domain = env!("AUTH0_DOMAIN");
+    let client_id = env!("AUTH0_CLIENT_ID");
+    format!("https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}")
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,25 @@
+use rand_os::rand_core::RngCore;
+
 pub fn log_string(value: &str) {
     gloo::console::log!(value);
+}
+
+pub fn create_random_string() -> String {
+    let mut random_numbers = [0u8; 43];
+    let charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_~.";
+    let charset_length = charset.len();
+
+    rand_os::rand_core::OsRng.fill_bytes(&mut random_numbers);
+
+    random_numbers
+        .into_iter()
+        .map(|random_number: u8| {
+            let (_index, random_char) = charset
+                .char_indices()
+                .nth(random_number as usize % charset_length)
+                .unwrap();
+            random_char.to_string()
+        })
+        .collect::<Vec<String>>()
+        .join("")
 }
